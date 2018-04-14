758cf4438e45ae729b7918f1052916d1761a463f
Marshall Ferguson <moferg01@louisville.edu>
2018-04-14 08:40:48 -0400

    Pseudocoded functions

--- js/app.js
+++ js/app.js
@@ -1,2 +1,15 @@
-// TODO: "Add to List" btn function
-// TODO: checkbox function
+function addToList() {
+    // select text input
+    // store text input data
+    // add input data to HTML
+}
+
+function crossOut() {
+    // select todo list item
+    // add crossout text decoration
+}
+
+function deleteFromList() {
+    // select todo list item(s) w/ crossout
+    // delete to do list item(s)
+}
